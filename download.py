662e6c81f28d481181731281bbc6872187625d81
Ekomobong <59149177+archie-bryann@users.noreply.github.com>
2026-02-24 16:42:46 +0100

    Modify yt-dlp format options for various resolutions
    Updated yt-dlp format options for video downloads.

--- download.py
+++ download.py
@@ -6,8 +6,14 @@ FFMPEG_PATH = os.path.join("C:", os.sep, "ffmpeg", "bin", "ffmpeg.exe")
 
 # yt-dlp configuration options
 ydl_opts = {
-    # 'format': 'bestvideo+bestaudio/best',        # Select best video and best audio (usually 8k)
-    'format': 'bestvideo[height<=2160]+bestaudio/best[height<=2160]', # 4k
+    # 'format': 'bestvideo+bestaudio/best',                     # Best available (up to 8K)
+    # 'format': 'bestvideo[height<=4320]+bestaudio/best',       # 8K
+    # 'format': 'bestvideo[height<=2160]+bestaudio/best[height<=2160]',  # 4K
+    # 'format': 'bestvideo[height<=1440]+bestaudio/best[height<=1440]',  # 2K (1440p)
+    'format': 'bestvideo[height<=1080]+bestaudio/best[height<=1080]',    # 1080p
+    # 'format': 'bestvideo[height<=720]+bestaudio/best[height<=720]',    # 720p
+    # 'format': 'bestvideo[height<=480]+bestaudio/best[height<=480]',    # 480p
+    # 'format': 'bestvideo[height<=360]+bestaudio/best[height<=360]',    # 360p
     'merge_output_format': 'mp4',                # Output format after merging
     'ffmpeg_location': FFMPEG_PATH,              # Path to ffmpeg executable
     'outtmpl': '%(title)s.%(ext)s',              # Output file naming template
